74cf3c8c97c5e15f5e3da1b680b36a995da79453
shota.kasai <shota_kasai@r.recruit.co.jp>
2023-04-24 11:59:00 +0000

    課題対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @pagy, @likes = pagy(@user.likes)
+    counts(@user)
+  end
+  
   private
   
   def user_params
